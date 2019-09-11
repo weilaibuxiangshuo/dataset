131d1ada211456ead04c510b1aebbacc013775f5
weilaibuxiangshuo <55013875+weilaibuxiangshuo@users.noreply.github.com>
2019-09-11 19:42:03 +0800

    Create example-git-window-course.py

---
+++ example-git-window-course.py
@@ -0,0 +1,13 @@
+先到项目下面，获取git连接，通过下载下来，再上传
+先切换到  d:/www
+cd d:/www
+执行下载
+git clone https://github.com/ceshi/x-admin2.45.git
+在d:/www下面会产生一个文件x-admin2.45
+把需在内容拷贝进去
+切换到当前目录下
+cd d:/www/x-admin2.45
+执行下
+git add .
+git commit -m "init"
+git push
