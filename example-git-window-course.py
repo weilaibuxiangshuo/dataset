7b903441da8be82c60961cb8e05d93c83eed349d
weilaibuxiangshuo <55013875+weilaibuxiangshuo@users.noreply.github.com>
2020-02-21 17:44:28 +0800

    Update example-git-window-course.py

--- example-git-window-course.py
+++ example-git-window-course.py
@@ -11,3 +11,11 @@ cd d:/www/x-admin2.45
 git add .
 git commit -m "init"
 git push
+
+
+下面是如何在windows下切换github账号
+因为，git安装时候有带git credentia
+第一次输入账号跟密码后，会保存，如果使用命令上传另一个github账号,会报错 
+remote:Permission to ...fatal:unable to access :The requestef URL returned error:403
+windows下需要到
+控制面板-用户账户-管理你的凭据-普通凭据-把git:https://github.com  删除掉，这样就可以正常上传了
